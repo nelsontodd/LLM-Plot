8737f490d5efd3447838541f4ae9e5e3f4dd658f
Nelson Morrow <nelsonmorrow@utexas.edu>
2023-05-30 06:07:30 +0000

    env template

--- env_template.sh
+++ env_template.sh
@@ -1,9 +1,16 @@
 export PROJECT_DIR=$(pwd)
 export ABS_SRC_DIR=$(realpath "src/")
 export OPENAI_API_KEY=""
-export OPENAI_USER=""
+export OPENAI_USER="Nelson Morrow"
 export OPENAI_MODEL="gpt-3.5-turbo"
 export PYTHONPATH="$PYTHONPATH:$ABS_SRC_DIR"
 export COINGECKO_API=""
+export TWITTER_ACCESS_TOKEN=""
+export TWITTER_ACCESS_TOKEN_SECRET=""
+export TWITTER_BEARER_TOKEN=""
+export TWITTER_API_KEY=""
+export TWITTER_API_KEY_SECRET=""
+export ETHERSCAN_API_KEY=""
 source .venv/bin/activate
 echo "PROJECT_DIR = "$PROJECT_DIR
+export ARKHAM_API_KEY=""
